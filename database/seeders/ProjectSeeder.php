e24fe2a7681908100338686b74ee8af194eb0935
RuxandraRambet <ruxandraramb@gmail.com>
2024-02-11 21:33:56 +0100

    feat: random types

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -15,12 +16,18 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
-        for($i = 0; $i < 10; $i++) {
-            $project = new Project();
+        Project::truncate();
 
+        for($i = 0; $i < 10; $i++) {
+            
+            $type = Type::inRandomOrder()->first();
+
+            $project = new Project();
+            
             $project->title = $faker->sentence(3);
             $project->description = $faker->text(500);
             $project->slug = Str::of($project->title)->slug('-');
+            $project->type_id = $type->id;
 
             $project->save();
 
